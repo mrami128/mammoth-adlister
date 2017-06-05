ca04b8a542dabb2d98ce68db2b482f35548a0c05
user <mramirez@usa.com>
2017-06-05 17:01:52 -0500

    initial on tomcat

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,21 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    protected void doGet(                            // represents input doGet or doPost
+            HttpServletRequest request, HttpServletResponse response
+        ) throws IOException {
+
+        response.setContentType("text/html");       // represents output
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World 2</h1>");
+        out.println("<p> This is example</p>");
     }
 }
