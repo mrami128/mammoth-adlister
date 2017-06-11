2ad17dc9cce730e0ce41fd69c94cae0df8154502
user <mramirez@usa.com>
2017-06-11 00:51:00 -0500

    before user input tag gets pulled

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,4 +1,6 @@
-public class DaoFactory {
+// as of FRIDAY pm
+//
+  public class DaoFactory {
     private static Ads adsDao;
 
     public static Ads getAdsDao() {
@@ -7,4 +9,19 @@ public class DaoFactory {
         }
         return adsDao;
     }
-}
+  }
+
+
+// latest version SATurday:
+//
+//  public class DaoFactory {
+//     private static Ads adsDao;
+//     private static Config config = new Config();
+//
+//    public static Ads getAdsDao() {
+//        if (adsDao == null) {
+//            adsDao = new MySQLAdsDao(config);
+//        }
+//        return adsDao;
+//    }
+//}
